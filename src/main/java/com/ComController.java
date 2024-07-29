eef48b0beb5c50d261e9e94582baccc20871e074
Artem <delux26rus@mail.ru>
2024-07-29 08:52:55 +0300

    SDK 11

--- src/main/java/com/ComController.java
+++ src/main/java/com/ComController.java
@@ -6,23 +6,49 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ComController implements Initializable {
-
+    private Path com;
     public ListView<String> mainListField;
     public TextField textFieldInput;
     private DataInputStream dis;
     private DataOutputStream dos;
+    private ComOutFiles comOutFiles;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+                com = Paths.get("C:/Education/Java4/Java4/com");
+                 Platform.runLater(()->{
+            try {
+                fuulFilesView();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        mainListField.setOnMouseClicked(e -> {
+                    if (e.getClickCount() == 2) {
+                        String fileName = mainListField.getSelectionModel().getSelectedItem();
+                        if (!Files.isDirectory(com.resolve(fileName))) {
+                            textFieldInput.setText(fileName);
+                        } else {
+                            textFieldInput.setText("File is not directory ");
+
+                        }
+                    }
+                }
+        );
+        });
+
         try {
             Socket socket = new Socket("127.0.0.1", 8089);
             dis = new DataInputStream(socket.getInputStream());
@@ -30,14 +56,16 @@ public class ComController implements Initializable {
             Thread read = new Thread(() -> {
                 try {
                     while (true) {
-                       String message =  dis.readUTF();
-                        Platform.runLater(() ->   mainListField.getItems().add(message));
+                        String message = dis.readUTF();
+                        Platform.runLater(() -> mainListField.getItems().addAll(comOutFiles.outFile()));
 
                     }
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
+            //showFiles();
+
             read.setDaemon(true);
             read.start();
         } catch (IOException e) {
@@ -50,5 +78,20 @@ public class ComController implements Initializable {
         dos.writeUTF(message);
         dos.flush();
         textFieldInput.clear();
+    }
+
+    public void showFiles() {
+        File folder = new File("/com");
+        for (File file : folder.listFiles()) {
+            String s = file.getName();
+            mainListField.getItems().add(s);
+        }
+    }
+
+    private void fuulFilesView() throws IOException {
+        mainListField.getItems().clear();
+        List<String> list = Files.list(com).map(p -> p.getFileName().
+                toString()).collect(Collectors.toList());
+        mainListField.getItems().addAll(list);
     }
 }
---
+++ src/main/java/com/CreatFile.java
@@ -0,0 +1,21 @@
+package com;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreatFile {
+    public static void main(String[] args) throws IOException {
+        File dir =  new File("C:/Education/Java4/Java4/com/com1");
+       if(!dir.exists()){
+ Files.createDirectory(Path.of("C:/Education/Java4/Java4/com/com1"));
+           System.out.println(dir.getAbsolutePath());
+           System.out.println("Great com1");
+           File f = new File(dir,"test1.txt");
+           f.createNewFile();
+           System.out.println("great file f");
+       }
+        System.out.println(dir.isDirectory());
+    }
+    }
